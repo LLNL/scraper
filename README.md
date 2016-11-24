da7d1e026be34c56fd32c18113ed3f9466e96bbd
Ian Lee <lee1001@llnl.gov>
2016-11-23 19:43:46 -0800

    Major updates and restructuring
    Getting ready for others to really be able to use

--- README.md
+++ README.md
@@ -2,6 +2,29 @@
 
 Scraper is a tool for scraping and visualizing open source data from GitHub.
 
+## Getting Started: Code.gov
+
+[Code.gov](https://code.gov) is a newly launched website of the US Federal
+Government to allow the People to access metadata from the governments custom
+developed software. This site requires metadata to function, and this Python
+library can help with that!
+
+To get started, you will need a GitHub Personal Auth Token to make requests to
+the GitHub API. This should be set in your environment or shell ``rc`` file with
+the name ``GITHUB_API_TOKEN``:
+
+    $ export GITHUB_API_TOKEN=1234567890abcdef1234567890abcdef
+
+    $ echo "export GITHUB_API_TOKEN=1234567890abcdef1234567890abcdef" >> ~/.bashrc
+
+
+To generate a ``code.json`` file for your GitHub organization:
+
+    $ pip install -r requirements.txt
+
+    $ ./scraper/gen_code_gov_json.py --agency <agency_name> --organization <organzation full name> --github-orgs <list of github org usernames ...>
+
+
 ## License
 
 Scraper is released under an MIT license. For more details see the
---
+++ scraper/code_gov/doe.py
@@ -0,0 +1,62 @@
+#! /usr/bin/env python
+
+def to_doe_csv(project):
+    """
+    Returns a comma separated string containing the following information
+
+    Headers from DOE Data Call:
+        - Agency
+        - Secretary Level
+        - Program Office
+        - Program/Office Component
+        - Project Name
+        - Project Description
+        - URL to Project License
+        - Is the Project Open source?
+        - Is Project Built for Government-Wide Reuse?
+        - Government-Wide Reuse Exemption Type
+        - Project Tags
+        - Project Point of Contact's Email
+        - Project Point of Contact's Name
+        - Project's Twitter URL
+        - Project Point of Contact's Phone Number
+        - Project Status
+        - Version Control System
+        - URL of the Public Project Repository
+        - URL of the Public Project Homepage
+        - URL to Download a Distribution of the Project
+        - Programming Languages Utilized
+        - Names of Project's Partnering Agencies
+        - Partner Agencies' Project Email
+        - Date of Last Metadata Updated
+        - Date of Last Commit to Project Repository
+        - Last Modified Date of Source Code or Package
+    """
+    return ';'.join([
+        '',
+        '',
+        '',
+        '',
+        str(project['name']),
+        str(project['description']),
+        str(project['license']),
+        str(project['openSourceProject']),
+        str(project['governmentWideReuseProject']),
+        str(project['exemption']),
+        str(project['tags']),
+        str(project['contact']['email']),
+        str(project['contact']['name']),
+        str(project['contact']['twitter']),
+        str(project['contact']['phone']),
+        str(project['status']),
+        str(project['vcs']),
+        str(project['repository']),
+        str(project['homepage']),
+        str(project['downloadURL']),
+        str(project['languages']),
+        str(project['partners']),
+        str(project['partners']),
+        str(project['updated']['metadataLastUpdated']),
+        str(project['updated']['lastCommit']),
+        str(project['updated']['sourceCodeLastModified']),
+    ])
