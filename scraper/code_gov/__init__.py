93bd8fbb83f5f93eab5aebefc53b23e50ec64d84
Ian Lee <lee1001@llnl.gov>
2017-02-26 13:45:53 -0800

    Updated stash -> project formatting

--- scraper/code_gov/__init__.py
+++ scraper/code_gov/__init__.py
@@ -240,11 +240,87 @@ class CodeGovProject(dict):
 
         Handles crafting Code.gov Project for Bitbucket Server repositories
         """
-        if not isinstance(repository, stashy.repos.Repository):
-            raise TypeError('Repository must be a stashy Repository object')
+        # if not isinstance(repository, stashy.repos.Repository):
+        #     raise TypeError('Repository must be a stashy Repository object')
+        if not isinstance(repository, dict):
+            raise TypeError('Repository must be a dict')
 
         project = klass()
 
-        ## Other processing --
+        # *name: [string] The project name
+        project['name'] = repository['name']
+
+        # *description: [string] A description of the project
+        description = repository['project'].get('description', 'Unknown')
+        project['description'] = 'Project Description: {}'.format(description)
+
+        # *license: [null or string] The URL of the project license, if available. null should be used if not.
+        project['license'] = None
+
+        # *openSourceProject: [integer] A value indicating whether or not the project is open source.
+        #   0: The project is not open source.
+        #   1: The project is open source.
+        project['openSourceProject'] = 0
+
+        # *governmentWideReuseProject: [integer] A value indicating whether or not the project is built for government-wide reuse.
+        #   0: The project is not built for government-wide reuse.
+        #   1: The project is built for government-wide reuse.
+        project['governmentWideReuseProject'] = 0
+
+        # *tags: [array] A list of string alphanumeric keywords that identify the project.
+        project['tags'] = []
+
+        # *contact: [object] Information about contacting the project.
+        #   *email: [string] An email address to contact the project.
+        #   name: [string] The name of a contact or department for the project
+        #   twitter: [string] The username of the project's Twitter account
+        #   phone: [string] The phone number to contact a project.
+        project['contact'] = {
+            'email': '',
+            'name': '',
+            'twitter': '',
+            'phone': '',
+        }
+
+        # status: [string] The development status of the project
+        #   "Ideation" - brainstorming phase.
+        #   "Alpha" - initial prototyping phase and internal testing.
+        #   "Beta" - a project is being tested in public.
+        #   "Production" - finished project, with development and maintenance ongoing.
+        #   "Archival" - finished project, but no longer actively maintained.
+        project['status'] = ''
+
+        # vcs: [string] A lowercase string with the name of the Version Control System in use on the project.
+        project['vcs'] = 'git'
+
+        # repository: [string] The URL of the public project repository
+        project['repository'] = repository.['links']['self'][0]['href']
+
+        # homepage: [string] The URL of the public project homepage
+        project['homepage'] = repository.homepage
+
+        # downloadURL: [string] The URL where a distribution of the project can be found.
+        project['downloadURL'] = ''
+
+        # languages: [array] A list of strings with the names of the programming languages in use on the project.
+        project['languages'] = []
+
+        # partners: [array] A list of strings containing the acronyms of agencies partnering on the project.
+        project['partners'] = []
+
+        # exemption: [integer] The exemption that excuses the project from government-wide reuse.
+        #   1: The sharing of the source code is restricted by law or regulation, including—but not limited to—patent or intellectual property law, the Export Asset Regulations, the International Traffic in Arms Regulation, and the Federal laws and regulations governing classified information.
+        #   2: The sharing of the source code would create an identifiable risk to the detriment of national security, confidentiality of Government information, or individual privacy.
+        #   3: The sharing of the source code would create an identifiable risk to the stability, security, or integrity of the agency's systems or personnel.
+        #   4: The sharing of the source code would create an identifiable risk to agency mission, programs, or operations.
+        #   5: The CIO believes it is in the national interest to exempt sharing the source code.
+        project['exemption'] = None
+
+        # updated: [object] Dates that the project and metadata have been updated.
+        #   metadataLastUpdated: [string] A date in YYYY-MM-DD or ISO 8601 format indicating when the metadata in this file was last updated.
+        #   lastCommit: [string] A date in ISO 8601 format indicating when the last commit to the project repository was.
+        #   sourceCodeLastModified: [string] A field intended for closed-source software and software outside of a VCS. The date in YYYY-MM-DD or ISO 8601 format that the source code or package was last updated.
+        # project['updated']['metadataLastUpdated'] = None
+        # project['updated']['lastCommit'] = None
 
         return project
