a729f9056c194717e593724022bcc2512aeea42b
Ian Lee <lee1001@llnl.gov>
2017-02-26 15:50:11 -0800

    Added support for using config file for repositories to scan

--- scraper/gen_code_gov_json.py
+++ scraper/gen_code_gov_json.py
@@ -2,6 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 import argparse
+import json
 import logging
 import os
 
@@ -54,32 +55,46 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Process some integers.')
 
-    parser.add_argument('--agency', type=str, nargs='?', default='UNKNOWN', help='Agency Label, e.g. "DOE"')
-    parser.add_argument('--organization', type=str, nargs='?', default='UNKNOWN', help='Organization Name')
+    parser.add_argument('--agency', type=str, nargs='?', default='', help='Agency Label, e.g. "DOE"')
+    parser.add_argument('--organization', type=str, nargs='?', default='', help='Organization Name')
 
-    parser.add_argument('--github-orgs', type=str, nargs='+', help='GitHub Organizations')
-    parser.add_argument('--github-repos', type=str, nargs='+', help='GitHub Repositories')
+    parser.add_argument('--config', type=str, nargs='?', default='', help='Configuration File (*.json)')
+
+    parser.add_argument('--github-orgs', type=str, nargs='+', default=[], help='GitHub Organizations')
+    parser.add_argument('--github-repos', type=str, nargs='+', default=[], help='GitHub Repositories')
 
     parser.add_argument('--to-csv', action='store_true', help='Toggle output to CSV')
 
     args = parser.parse_args()
 
-    agency = args.agency
-    organization = args.organization
-    org_names = args.github_orgs or []
-    repo_names = args.github_repos or []
+    try:
+        config_json = json.load(open(args.config))
+    except (FileNotFoundError, json.JSONDecodeError):
+        if args.config:
+            raise
+        config_json = {}
+
+    agency = config_json.get('agency', 'UNKNOWN')
+    organization = config_json.get('organization', 'UNKNOWN')
+    github_orgs = config_json.get('github_orgs', [])
+    github_repos = config_json.get('github_repos', [])
+
+    agency = args.agency or agency
+    organization = args.organization or organization
+    github_orgs.extend(args.github_orgs)
+    github_repos.extend(args.github_repos)
 
     logger.debug('Agency: %s', agency)
     logger.debug('Organization: %s', organization)
-    logger.debug('GitHub.com Organizations: %s', org_names)
-    logger.debug('GitHub.com Repositories: %s', repo_names)
+    logger.debug('GitHub.com Organizations: %s', github_orgs)
+    logger.debug('GitHub.com Repositories: %s', github_repos)
 
     code_json = CodeGovMetadata(agency, organization)
 
-    for org_name in org_names:
+    for org_name in github_orgs:
         code_json['projects'].extend(process_organization(org_name))
 
-    for repo_name in repo_names:
+    for repo_name in github_repos:
         code_json['projects'].append(process_repository(repo_name))
 
     str_org_projects = code_json.to_json()
