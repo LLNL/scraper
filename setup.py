1827579d21ec654e3254469a12d5fad9a6221f26
Ian Lee <lee1001@llnl.gov>
2017-02-26 14:56:27 -0800

    Added setup.py to package

---
+++ setup.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='llnl-scraper',
+    version='0.1.0',
+    description='Package for extracting ',
+    author='Ian Lee',
+    author_email='lee1001@llnl.gov',
+    url='https://github.com/llnl/scraper',
+    packages=find_packages(),
+    install_requires=[
+        'github3.py>=1.0.0a4',
+        'python-gitlab>=0.17',
+        'stashy>=0.3',
+    ],
+    scripts=[
+        'scraper/gen_code_gov_json.py',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+)
