ad6e68a2fd1f6779c7aaf880ee789920cea0540d
Ian Lee <lee1001@llnl.gov>
2018-03-12 09:53:47 -0700

    Added missing utility file

---
+++ scraper/util.py
@@ -0,0 +1,19 @@
+import logging
+import os
+
+from subprocess import Popen, PIPE, STDOUT
+
+logger = logging.getLogger(__name__)
+
+
+def execute(command, cwd=None):
+    logger.debug('Forking command: %s', command)
+
+    if cwd is None:
+        cwd = os.getcwd()
+    elif not os.path.isdir(cwd):
+        raise ValueError('path does not exist: %s', cwd)
+
+    process = Popen(command, cwd=cwd, stdout=PIPE, stderr=STDOUT)
+    out, err = process.communicate()
+    return str(out), str(err)
