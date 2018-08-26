f9c5d45ab0d454b5d936f98ad66a0610874aecdd
Ian Lee <lee1001@llnl.gov>
2018-08-25 21:42:15 -0700

    Realized that "internal" projects require being authenticated, so not really opensource...

--- scraper/code_gov/models.py
+++ scraper/code_gov/models.py
@@ -13,7 +13,7 @@ from scraper.util import _prune_dict_null_str, labor_hours_from_url
 
 logger = logging.getLogger(__name__)
 
-POLICY_START_DATE = date_parse('2016-08-08')
+POLICY_START_DATE = date_parse('2016-08-08T00:00:00Z')
 
 
 class Metadata(dict):
@@ -281,7 +281,7 @@ class Project(dict):
         web_url = repository.web_url
         public_server = web_url.startswith('https://gitlab.com')
 
-        if repository.visibility in ('public', 'internal') and public_server:
+        if repository.visibility in ('public') and public_server:
             project['permissions']['usageType'] = 'openSource'
         elif date_parse(repository.created_at) < POLICY_START_DATE:
             project['permissions']['usageType'] = 'exemptByPolicyDate'
