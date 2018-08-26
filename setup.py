2a3f0ea27c38d115293cec83233d4a2a280b6517
Ian Lee <lee1001@llnl.gov>
2018-08-25 23:49:00 -0700

    Release version 0.4.0
    Major updates include:
    
    - Added GitLab support
    - Added Bitbucket Server support
    - Added repo exclusion support
    - Refactored / modular design

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='llnl-scraper',
-    version='0.4.0-dev',
+    version='0.4.0',
     description='Package for extracting software repository metadata',
     author='Ian Lee',
     author_email='lee1001@llnl.gov',
