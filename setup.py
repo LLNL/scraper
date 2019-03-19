881a316e4c04dfa5a9cf491b7c7f9f997a7c56ea
Ian Lee <lee1001@llnl.gov>
2019-03-18 20:08:33 -0700

    Bumped version post release

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('requirements/production.txt') as fp:
 
 setup(
     name='llnl-scraper',
-    version='0.7.0',
+    version='0.8.0-dev',
     description='Package for extracting software repository metadata',
     long_description=long_description,
     author='Ian Lee',
